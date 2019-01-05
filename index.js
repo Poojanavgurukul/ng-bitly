e7224ba8ac0e8c9b1408851700b6d94ad93e7731
Poojanavgurukul <pooja17@navgurukul.org>
2019-01-05 15:13:03 +0530

    add features

--- index.js
+++ index.js
@@ -25,34 +25,51 @@ const loggerMiddleware = (req, res, next) => {
 app.use(loggerMiddleware)
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    return res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     return res.send('Hello World!')
+// })
 
 app.post('/shorten', (req, res) => {
+    const hash = shortid.generate();
     URL.findOne({url: req.body.url}).exec()
     .then(existingUrl => {
-       if (existingUrl) {
-            return existingUrl;
-       } else {
-            const hash = shortid.generate();
-            return URL.create({ hash: hash, url: req.body.url });
-       }
-    }).then(doc => {
+        if (existingUrl) {
+                return existingUrl;
+        } else {
+                const hash = shortid.generate();
+                return URL.create({ hash: hash, url: req.body.url });
+        }
+    })
+    .then(doc => {
         return res.status(201).send(doc)
     })
 })
-
+app.get('/hits',(req,res)=>{
+    console.log(req.query)
+    URL.findOne({hash:req.query.hash}).exec()
+        .then(existingUrl=>{
+            if(existingUrl){
+                return res.status(200).send({hits:existingUrl.hits})
+            }else{
+                return res.send(404)
+            }
+        })
+})
 app.get('/:hash', (req, res) => {
     URL.findOne({hash: req.params.hash}).exec()
-    .then(existingUrl => {
-        if (existingUrl) {
-            console.log("Redirecting...")
-            return res.redirect(existingUrl.url);
-        } else {
-            return res.status(404)
-        }
-    })
+        .then(existingUrl => {
+            if (existingUrl) {
+                console.log("Redirecting...")
+                return URL.update({hash: req.params.hash},{$set:{hits: existingUrl.hits+1}}).exec()
+                            .then(() => {
+                                console.log(existingUrl)
+                                return res.redirect(existingUrl.url);
+                            })
+            } else {
+                return res.send(404)
+            }
+        })
 })
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- test.js
+++ test.js
@@ -1,43 +1,60 @@
-const expect = require('chai').expect
-const axios = require('axios')
+const expect = require("chai").expect;
+const axios = require("axios");
 
 describe("API Tests", function() {
-  let id, hash;
+  let id, hash, hits;
   describe("/shorten", function() {
-    
-    it("should return a shortened URL", (done) => {
-        const request = { url: "https://google.com" }
-        axios.post('http://localhost:3000/shorten', request)
-        .then(response => {
-            id = response.data._id
-            hash = response.data.hash
-            expect(response.status).to.equal(201)
-            expect(response.data._id).to.be.ok
-            expect(response.data.hash).to.be.ok;
-            done()
-        })
+    it("should return a shortened URL", done => {
+      const request = { url: "https://google.com" };
+      axios.post("http://localhost:3000/shorten", request).then(response => {
+        id = response.data._id;
+        hash = response.data.hash;
+        hits = response.data.hits;
+        expect(response.status).to.equal(201);
+        expect(response.data._id).to.be.ok;
+        expect(response.data.hash).to.be.ok;
+        done();
+      });
     });
-    it("should return the same shortened URL for the earlier url", (done) => {
-        const request = { url: "https://google.com" }
-        axios.post('http://localhost:3000/shorten', request)
-        .then(response => {
-            expect(response.status).to.equal(201)
-            expect(response.data._id).to.equal(id)
-            expect(response.data.hash).to.equal(hash)
-            done()
-        })
+    it("should return the same shortened URL for the earlier url", done => {
+      const request = { url: "https://google.com" };
+      axios.post("http://localhost:3000/shorten", request).then(response => {
+        expect(response.status).to.equal(201);
+        expect(response.data._id).to.equal(id);
+        expect(response.data.hash).to.equal(hash);
+        done();
+      });
     });
   });
 
   describe("/:hash", function() {
-
-    it("should redirect the user for an existing hash", (done) => {
-      axios.get("http://localhost:3000/"+hash)
-      .then(response => {
-        expect(response.data).to.contain("google.com")
-        done()
-      })
-    })
-  })
-
+    it("should redirect the user for an existing hash", done => {
+      axios.get("http://localhost:3000/" + hash).then(response => {
+        expect(response.data).to.contain("google.com");
+        done();
+      });
+    });
+    it("hash is not exist", done => {
+      axios.get(`http://localhost:3000/Viaga8eiSS`).catch(error => {
+        expect(error.response.status).to.equal(404);
+        done();
+      });
+    });
+    it("hits", done => {
+      const request = { url: "https://google.com" };
+      axios.post("http://localhost:3000/shorten", request).then(response => {
+        expect(response.data.hits).to.equal(hits + 1);
+        done();
+      });
+    });
+  });
+  describe("/hits", function() {
+    it("number of hits", done => {
+      axios.get(`http://localhost:3000/hits?hash=${hash}`).then(response => {
+        expect(response.data.hits).to.equal(hits + 1);
+        done();
+      });
+    });
+  });
 });
+
